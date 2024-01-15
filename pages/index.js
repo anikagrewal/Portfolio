1237497c997b61ceac289c825aa36e0abaf0172f
anikagrewal <a.grewal.work@gmail.com>
2024-01-15 09:27:03 -0800

    portfolio

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from '@/styles/Home.module.css'
+
+
+
+export default function Home() {
+  return (
+    <>
+      
+      <main className={`${styles.main}`}>
+      <div>
+        <h1>Welcome!</h1>
+        <h3>This site is currently in builiding process.</h3>
+      </div>
+      </main>
+    </>
+  )
+}
