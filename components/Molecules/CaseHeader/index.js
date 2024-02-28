8421d7a2d58c1cfdfffbf815a3d4e66ebe056066
anikagrewal <a.grewal.work@gmail.com>
2024-02-28 05:34:09 -0800

    case study fixes

--- components/Molecules/CaseHeader/index.js
+++ components/Molecules/CaseHeader/index.js
@@ -13,6 +13,11 @@ return (
       <p className={styles.desc}>Innersight is a journal app that utilizes artificial intelligence to help find problems in your day to day life and give insights on how to covercome these struggles.<br className={styles.space} />Your personal mental health compaion.</p>
       <p className={styles.desc}>Roles: Project Manager | Developer</p>
       <p className={styles.desc}>Time Frame: Sept 2023 - Ongoing</p>
+    
+        <a className={styles.linkText} href="https://play.google.com/store/apps/details?id=ca.innersightapp.app&pcampaignid=web_share">
+       <button className={styles.btn}>Visit Live Beta</button>
+        </a>
+      
       </div>
       <Image src={'/images/innerSight.svg'} alt='logo' width={519} height={453} />
     </div>
